c7b5ca1e3f4f3a41cbe22a0811b66e23c5eae3af
Gene Culling <culling66@gmail.com>
2022-04-14 23:55:04 +1200

    Add Velocity Macro to define output

--- src/main/java/com/geneculling/javakata/servlet/FilemetadataServlet.java
+++ src/main/java/com/geneculling/javakata/servlet/FilemetadataServlet.java
@@ -3,31 +3,31 @@ package com.geneculling.javakata.servlet;
 //import org.apache.velocity.Template;
 //import org.apache.velocity.VelocityContext;
 //import org.apache.velocity.app.VelocityEngine;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.templaterenderer.TemplateRenderer;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 //import java.io.StringWriter;
 
 public class FilemetadataServlet extends HttpServlet {
 
+     private final TemplateRenderer renderer;
+
+    @Autowired
+    public FilemetadataServlet(@ComponentImport TemplateRenderer renderer){
+        this.renderer = renderer;
+    }
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().write("Hello World");
-        response.flushBuffer();
-
-
-
-//        VelocityEngine velocityEngine = new VelocityEngine();
-//        velocityEngine.init();
-//        VelocityContext context = new VelocityContext();
-//        Template template = velocityEngine.getTemplate( "templates/hello.vm" );
-//        StringWriter writer = new StringWriter();
-//        template.merge( context, writer );
-//
-//        response.getWriter().write( writer.toString() );
-//        response.flushBuffer();
+        Map<String, Object> map = new HashMap<>();
+        renderer.render("templates/hello.vm", map, response.getWriter());
     }
 
 }
